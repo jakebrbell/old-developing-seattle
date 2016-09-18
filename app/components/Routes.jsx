36040d168c28f8ecab889ffdbdff939c7eebc5cc
Jake Bell <jakebrbell@gmail.com>
2016-09-18 07:26:54 -0700

    Close #13 Create OrgProfileFull component

--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -3,6 +3,7 @@ import App from 'components/App';
 import Home from 'components/Home';
 import Login from 'components/Login';
 import OrgList from 'components/OrgList';
+import OrgProfileFull from 'components/OrgProfileFull';
 import React from 'react';
 import Register from 'components/Register';
 
@@ -12,6 +13,7 @@ class Routes extends React.Component {
       <Route component={App} path="/">
         <IndexRoute component={Home} />
         <Route component={OrgList} path="/organizations" />
+        <Route component={OrgProfileFull} path="/organizations/number" />
         <Route component={Login} path="/login" />
         <Route component={Register} path="/register" />
       </Route>
