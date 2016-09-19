21b14b360613016c66cf16c19776cd6a4e323ee3
Jake Bell <jakebrbell@gmail.com>
2016-09-19 10:44:19 -0700

    Close #33 Create server.js file

---
+++ server.js
@@ -0,0 +1,84 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// const users = require('./routes/users');
+// const orgs = require('./routes/orgs');
+
+app.disable('x-powered-by');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CSRF protection
+app.use('/api', (req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+app.use(bodyParser.json());
+
+// app.use('/api', users);
+// app.use('/api', orgs);
+
+app.use('/api', (_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status) {
+    return res
+      .status(err.status)
+      .send(err);
+  }
+  
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
