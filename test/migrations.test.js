66e620994090a72191035cef59f3c42019ab47eb
Jake Bell <jakebrbell@gmail.com>
2016-09-19 14:10:21 -0700

    Close #23 Create migrations test file

---
+++ test/migrations.test.js
@@ -0,0 +1,300 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const { assert } = require('chai');
+const { suite, test } = require('mocha');
+const knex = require('../db/knex');
+
+suite('Migrations', () => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('users columns', (done) => {
+    knex('users').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'users_id_seq\'::regclass)'
+          },
+
+          first_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          last_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          email: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: null
+          },
+
+          hashed_password: {
+            type: 'character',
+            maxLength: 60,
+            nullable: false,
+            defaultValue: null
+          },
+
+          title: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          causes: {
+            type: 'json',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'[]\'::json'
+          },
+
+          skills: {
+            type: 'json',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'[]\'::json'
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('orgs columns', (done) => {
+    knex('orgs').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'orgs_id_seq\'::regclass)'
+          },
+
+          name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          desc: {
+            type: 'text',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'\'::text'
+          },
+
+          email: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: null
+          },
+
+          causes: {
+            type: 'json',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'[]\'::json'
+          },
+
+          logo_url: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          proposals: {
+            type: 'json',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'[]\'::json'
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('orgs_users columns', (done) => {
+    knex('orgs_users').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'orgs_users_id_seq\'::regclass)'
+          },
+
+          org_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          user_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          messages: {
+            type: 'json',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'[]\'::json'
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('orgs_users constraints', (done) => {
+    const query = `
+      SELECT
+        tc.table_name, kcu.column_name,
+        ccu.table_name AS foreign_table_name,
+        ccu.column_name AS foreign_column_name
+      FROM
+        information_schema.table_constraints AS tc
+        JOIN information_schema.key_column_usage AS kcu
+          ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu
+          ON ccu.constraint_name = tc.constraint_name
+      WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='orgs_users';
+    `;
+
+    knex.raw(query)
+      .then((result) => {
+        const actual = result.rows;
+
+        const expected = [{
+          table_name: 'orgs_users',
+          column_name: 'org_id',
+          foreign_table_name: 'orgs',
+          foreign_column_name: 'id'
+        }, {
+          table_name: 'orgs_users',
+          column_name: 'user_id',
+          foreign_table_name: 'users',
+          foreign_column_name: 'id'
+        }];
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
