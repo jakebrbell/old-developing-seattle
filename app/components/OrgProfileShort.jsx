f20d2e9fa5dcda835ff24e71407a305b39b5b811
Jake Bell <jakebrbell@gmail.com>
2016-09-21 11:39:42 -0700

    Refactor OrgProfileShort component to add container

--- app/components/OrgProfileShort.jsx
+++ app/components/OrgProfileShort.jsx
@@ -1,63 +1,60 @@
-import { Col, Image, Label, Row } from 'react-bootstrap';
+import { Button, Col, Image, Label, Row } from 'react-bootstrap';
 import { Link } from 'react-router';
 import React from 'react';
 
-class OrgProfileShort extends React.Component {
-  render() {
-    const { org } = this.props;
+const OrgProfileShort = ({ org }) => {
+  const styles = {
+    label: {
+      backgroundColor: '#8ED4CC',
+      boxShadow: '3px 3px 7px -1px rgba(0,0,0,0.52)'
+    },
+    labelWrapper: {
+      display: 'inline',
+      marginRight: '10px'
+    },
+    link: {
+      color: 'black'
+    },
+    logo: {
+      display: 'block',
+      height: '200px',
+      margin: '20px auto',
+      background: '#fff'
+    },
+    row: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      borderRadius: '10px',
+      marginBottom: '30px',
+      padding: '30px'
+    }
+  };
 
-    const styles = {
-      label: {
-        backgroundColor: '#8ED4CC',
-        boxShadow: '3px 3px 7px -1px rgba(0,0,0,0.52)'
-      },
-      labelWrapper: {
-        display: 'inline',
-        marginRight: '10px'
-      },
-      link: {
-        color: 'black'
-      },
-      logo: {
-        display: 'block',
-        height: '200px',
-        margin: '20px auto',
-        background: '#fff'
-      },
-      row: {
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        borderRadius: '10px',
-        marginBottom: '30px',
-        padding: '30px'
-      }
-    };
-
-    return <Row style={styles.row}>
-      <Col xs={12}>
-        <Link to={'/'} style={styles.link}>
-          <Col xs={12} sm={3} style={{ padding: 0 }}>
-            <Image
-              circle
-              src={`images/${org.logo_url}`}
-              style={styles.logo}
-            />
-          </Col>
-          <Col xs={12} sm={9} style={{ paddingLeft: 50 }}>
-            <h2>{org.name}</h2>
-            <p>{org.address}</p>
-            <div style={{ marginBottom: 15 }}>
-              {org.causes.map((cause, index) => {
-                return <h3 key={index} style={styles.labelWrapper}>
-                  <Label style={styles.label}>{cause}</Label>
-                </h3>;
-              })}
-            </div>
-            <p>{org.desc}</p>
-          </Col>
-        </Link>
-      </Col>
-    </Row>;
-  }
+  return <Row style={styles.row}>
+    <Col xs={12}>
+      <Link to={`/organizations/${org.id}`} style={styles.link}>
+        <Col xs={12} sm={3} style={{ padding: 0 }}>
+          <Image
+            circle
+            src={`/images/${org.logoUrl}`}
+            style={styles.logo}
+          />
+          <Button block>View Full Profile</Button>
+        </Col>
+        <Col xs={12} sm={9} style={{ paddingLeft: 50 }}>
+          <h2>{org.name}</h2>
+          <p>{org.address}</p>
+          <div style={{ marginBottom: 15 }}>
+            {org.causes.map((cause, index) => {
+              return <h3 key={index} style={styles.labelWrapper}>
+                <Label style={styles.label}>{cause}</Label>
+              </h3>;
+            })}
+          </div>
+          <p>{org.desc}</p>
+        </Col>
+      </Link>
+    </Col>
+  </Row>;
 }
 
 export default OrgProfileShort;
