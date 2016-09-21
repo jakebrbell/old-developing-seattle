d759f46b9ff5a023774cfe12b234a27c5eb07c67
Jake Bell <jakebrbell@gmail.com>
2016-09-21 11:39:25 -0700

    Refactor OrgProfileFull component to add container

--- app/components/OrgProfileFull.jsx
+++ app/components/OrgProfileFull.jsx
@@ -2,24 +2,24 @@ import { Button, Col, Grid, Label, Image, Row, Well } from 'react-bootstrap';
 import Proposal from 'components/Proposal';
 import React from 'react';
 
-const org = {
-  name: 'Company Name',
-  address: '1260 Republican Street, Seattle, WA 98109',
-  logo: 'https://s-media-cache-ak0.pinimg.com/564x/5a/bd/f4/5abdf4f571593356b4a27339529798d0.jpg',
-  causes: ['Animal Welfare', 'Environment'],
-  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  proposals: [{
-    name: 'We need a new website',
-    description: 'Our website is outdated and no longer working. We are looking for someone who can build us a new one.',
-    skills: ['Designer', 'Web Developer']
-  }, {
-    name: 'We need a new website',
-    description: 'Our website is outdated and no longer working. We are looking for someone who can build us a new one.',
-    skills: ['Designer', 'Web Developer']
-  }]
-}
+// const org = {
+//   name: 'Company Name',
+//   address: '1260 Republican Street, Seattle, WA 98109',
+//   logo: 'https://s-media-cache-ak0.pinimg.com/564x/5a/bd/f4/5abdf4f571593356b4a27339529798d0.jpg',
+//   causes: ['Animal Welfare', 'Environment'],
+//   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+//   proposals: [{
+//     name: 'We need a new website',
+//     description: 'Our website is outdated and no longer working. We are looking for someone who can build us a new one.',
+//     skills: ['Designer', 'Web Developer']
+//   }, {
+//     name: 'We need a new website',
+//     description: 'Our website is outdated and no longer working. We are looking for someone who can build us a new one.',
+//     skills: ['Designer', 'Web Developer']
+//   }]
+// }
 
-const OrgProfileFull = () => {
+const OrgProfileFull = ({ org }) => {
   const styles = {
     label: {
       backgroundColor: '#8ed4cc',
@@ -50,7 +50,7 @@ const OrgProfileFull = () => {
       <Col xs={12} sm={3}>
         <Image
           circle
-          src={org.logo}
+          src={`/images/${org.logoUrl}`}
           style={styles.logo}
         />
       </Col>
@@ -62,7 +62,7 @@ const OrgProfileFull = () => {
             <Label style={styles.label}>{cause}</Label>
           </h3>;
         })}
-        <p style={{ marginTop: '20px' }}>{org.description}</p>
+        <p style={{ marginTop: '20px' }}>{org.desc}</p>
       </Col>
       <Col xs={12}>
         {org.proposals.map((proposal, index) => {
