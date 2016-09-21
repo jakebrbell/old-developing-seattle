6ffc864c131a814eb211aa15f59103e8ad22b54b
Jake Bell <jakebrbell@gmail.com>
2016-09-21 11:36:23 -0700

    Create OrgProfileFullContainer component

---
+++ app/components/OrgProfileFullContainer.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import OrgProfileFull from 'components/OrgProfileFull';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import ReactDOM from 'react-dom';
+
+class OrgProfileFullContainer extends Component {
+  componentWillMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const { orgs, routeParams } = this.props;
+    const orgId = Number.parseInt(routeParams.orgId);
+    let currentOrg;
+
+    if (orgs.length > 0) {
+      for (const org of orgs) {
+        if (org.id === orgId) {
+          currentOrg = org;
+        }
+      }
+
+      return <OrgProfileFull org={currentOrg}/>;
+    }
+
+    return <div></div>;
+
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    orgs: state.orgs
+  };
+};
+
+export default withRouter(connect(
+  mapStateToProps
+)(OrgProfileFullContainer));
