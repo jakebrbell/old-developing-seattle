d6d56accad928e1bbdbf778c12b465581d1ac624
Jake Bell <jakebrbell@gmail.com>
2016-09-21 11:38:10 -0700

    Add actions for fetching orgs from database

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,4 +1,21 @@
+import axios from 'axios';
+
 export const addUser = (firstName) => ({
   type: 'ADD_USER',
   firstName
 });
+
+const receiveOrgs = (response) => ({
+  type: 'RECEIVE_ORGS',
+  response
+});
+
+export const fetchOrgs = () => (dispatch) => {
+  axios.get('/api/orgs')
+    .then((res) => {
+      dispatch(receiveOrgs(res.data));
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
