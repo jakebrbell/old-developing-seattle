4a30722098228b75e5e3c255a7eefd21a6922361
Jake Bell <jakebrbell@gmail.com>
2016-09-23 16:00:10 -0700

    Refactor Login component

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,49 +1,81 @@
 import { Button, Col, ControlLabel, Form, FormControl, FormGroup, Grid, Row, Well } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { loginUser } from '../actions';
 
-class Login extends React.Component {
-  render() {
-    return <Grid style={{ marginTop: '100px' }}>
-      <Row>
-        <Col xs={12} sm={6} smOffset={3} style={{ marginTop: '80px' }}>
-          <Well bsSize="large">
-            <h1 style={{ textAlign: 'center', margin: '10px 0 30px' }}>Login</h1>
-            <p style={{ textAlign: 'center', marginBottom: '25px' }}>Not registered yet? {' '}
-              <Link to={'/register'}>Register here.</Link>
-            </p>
-            <Form horizontal>
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Email
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="email" placeholder="Email" />
-                </Col>
-              </FormGroup>
+const Login = ({ handleSubmit, dispatch, router }) => {
+  const handleLoginFormSubmit = (formFields) => {
+    dispatch(loginUser(formFields));
+    router.push('/');
+  };
 
-              <FormGroup controlId="formHorizontalPassword">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Password
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="password" placeholder="Password" />
-                </Col>
-              </FormGroup>
+  const styles = {
+    field: {
+      width: '100%',
+      borderRadius: '4px',
+      padding: '6px 12px',
+      border: '1px solid #CCC',
+      display: 'block',
+      fontSize: '14px',
+      color: '#555'
+    }
+  };
 
-              <FormGroup>
-                <Col smOffset={2} sm={10}>
-                  <Button type="submit">
-                    Sign in
-                  </Button>
-                </Col>
-              </FormGroup>
-            </Form>
-          </Well>
-        </Col>
-      </Row>
-    </Grid>;
-  }
-}
+  return <Grid style={{ marginTop: '100px' }}>
+    <Row>
+      <Col xs={12} sm={6} smOffset={3} style={{ marginTop: '80px' }}>
+        <Well bsSize="large">
+          <h1 style={{ textAlign: 'center', margin: '10px 0 30px' }}>Login</h1>
+          <p style={{ textAlign: 'center', marginBottom: '25px' }}>Not registered yet? {' '}
+            <Link to={'/register'}>Register here.</Link>
+          </p>
+          <Form
+            horizontal
+            onSubmit={handleSubmit(handleLoginFormSubmit)}
+          >
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Email
+              </Col>
+              <Col sm={10}>
+                <Field
+                  component="input"
+                  name="email"
+                  placeholder="Email"
+                  style={styles.field}
+                  type="email"
+                />
+              </Col>
+            </FormGroup>
 
-export default Login;
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Password
+              </Col>
+              <Col sm={10}>
+                <Field
+                  component="input"
+                  name="password"
+                  placeholder="Password"
+                  style={styles.field}
+                  type="password"
+                />
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit" bsStyle="primary" style={{ float: 'right', width: '50%', marginTop: '20px' }}>
+                  Sign in
+                </Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </Well>
+      </Col>
+    </Row>
+  </Grid>;
+};
+
+export default withRouter(reduxForm({ form: 'login' })(Login));
